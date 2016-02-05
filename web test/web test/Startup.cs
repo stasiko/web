6edb30e0757f02ee24cbaac85bd09c6814e3a201
Stanislav Lebedenko <s.lebedenko@altsoft.com.ua>
2016-02-05 17:48:28 +0200

    test mvc5 project

---
+++ web test/web test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(web_test.Startup))]
+namespace web_test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
